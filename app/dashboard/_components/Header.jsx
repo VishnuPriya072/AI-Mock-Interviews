f4a351c2088fed57ced8e61d43cb92a8b5c9f97c
Vishnu Priya <vishnupriyaazhagarangan07@gmail.com>
2024-08-25 17:14:58 +0530

    day 2 code

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,23 +1,36 @@
+"use client"
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 function Header() {
+  const path=usePathname();
+  useEffect(()=>{
+    console.log(path)
+  },[])
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
       <Image src={'/logo.svg'}width={160} height={100} alt='logo'/>
-      <ul className='flex gap-6' > 
-        <li className='hover:text-primary hover:font-bold transition-all'>
-          Dashboard
-        </li>
-        <li>Question</li>
-        <li>Upgrade</li>
-        <li>
+      <ul className='flex gap-6'>
+        <li className={`hover:text-pink-500 hover:font-bold transition-all cursor-pointer
+        ${path==='/dashboard'&&'text-pink-500'}
+        `}
+        >Dashboard</li>
+        <li className={`hover:text-pink-500 hover:font-bold transition-all cursor-pointer
+          ${path=='/dashboard/question'&&'text-pink-500'}`}>Question</li>
+        <li className={`hover:text-pink-500 hover:font-bold transition-all cursor-pointer
+          ${path=='/dashboard/upgrade'&&'text-pink-500'}`}> Upgrade</li>
+        <li className={`hover:text-pink-500 hover:font-bold transition-all cursor-pointer
+          ${path=='/dashboard/how'&& 'text-pink-500'
+          }`}>
           How its Work?
         </li>
       </ul>
+        
+      
       <UserButton/>
-      </div>
+    </div>
   )
 }
 
